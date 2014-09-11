ce08b465b658c4e886d49ec33361cee767f86db6
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-09-11 12:57:58 +0200

    Additional test cases: PNGs, some with transparency, and JPG with colour profile to ignore

--- test.sh
+++ test.sh
@@ -6,9 +6,9 @@ d=`dirname "$0"`
 
 cd "$d/testdata"
 echo
-../build/blockhash -b 16 *.jpg | diff -s exact-hashes.txt -
+../build/blockhash -b 16 *.jpg *.png | sort | diff -su exact-hashes.txt -
 echo
-../build/blockhash -q -b 16 *.jpg | diff -s quick-hashes.txt -
+../build/blockhash -q -b 16 *.jpg *.png | sort | diff -su quick-hashes.txt -
 echo
 echo OK
 echo
