13677c4800e46120286b5da5b5ceb5f97ee16f47
Jonas Oberg <jonas@ffkp.se>
2015-11-10 14:49:33 -0500

    Added test cases for videos

--- test.sh
+++ test.sh
@@ -4,14 +4,16 @@ set -e
 
 d=`dirname "$0"`
 
-cd "$d/testdata"
+(cd "$d/testdata/images"
 
-echo
-../build/blockhash -b 16 *.jpg *.png | sort | diff -su exact-hashes.txt -
+../../build/blockhash -b 16 *.jpg *.png | sort | diff -su exact-hashes.txt -
+../../build/blockhash -q -b 16 *.jpg *.png | sort | diff -su quick-hashes.txt -
+)
 
-echo
-../build/blockhash -q -b 16 *.jpg *.png | sort | diff -su quick-hashes.txt -
-
+(cd "$d/testdata/videos"
+../../build/blockhash_video -b 16 *.mpeg *.ogv | sort | diff -su exact-hashes.txt -
+../../build/blockhash_video -q -b 16 *.mpeg *.ogv | sort | diff -su quick-hashes.txt -
+)
 echo
 echo Test finished.
 echo
