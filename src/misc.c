389817251c8b496129871f6aed3e77289e19d90a
Ivan Pizhenko <ivanp2015@users.noreply.github.com>
2015-11-29 09:39:00 +0200

    video-hash-ffmpeg progress

---
+++ src/misc.c
@@ -0,0 +1,25 @@
+/*
+ * Perceptual image hash calculation tool based on algorithm descibed in
+ * Block Mean Value Based Image Perceptual Hashing by Bian Yang, Fan Gu and Xiamu Niu
+ *
+ * Copyright 2014-2015 Commons Machinery http://commonsmachinery.se/
+ * Distributed under an MIT license, please see LICENSE in the top dir.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include "processing.h"
+
+void debug_print_hash(const int* hash, int bits) 
+{
+    int i;
+    for (i = 0; i < bits * bits; i++) {
+        if (i != 0 && i % bits == 0)
+            printf("\n");
+        printf("%d", hash[i]);
+    }
+    printf("\n");
+}
--- test.sh
+++ test.sh
@@ -5,10 +5,31 @@ set -e
 d=`dirname "$0"`
 
 cd "$d/testdata"
+
+hash_tool=../src/blockhash
+
 echo
-../build/blockhash -b 16 *.jpg *.png | sort | diff -su exact-hashes.txt -
+echo Testing exact image hashes...
 echo
-../build/blockhash -q -b 16 *.jpg *.png | sort | diff -su quick-hashes.txt -
+${hash_tool}  -b 16 *.jpg *.png | sort | diff -su image-exact-hashes.txt -
+
 echo
-echo OK
+echo Testing quick image hashes...
 echo
+${hash_tool} -q -b 16 *.jpg *.png | sort | diff -su image-quick-hashes.txt -
+
+echo
+echo Testing exact video hashes...
+echo
+${hash_tool} -b 16 -V *.mp4 | sort | diff -su video-exact-hashes.txt -
+
+echo
+echo Testing quick video hashes...
+echo
+${hash_tool} -q -b 16 -V *.mp4 | sort | diff -su video-quick-hashes.txt -
+
+
+echo
+echo Test finished.
+echo
+
